9427f5ed3885f8ea60b8199b10cb3d34a6d22089
gmjishara <janith971231@gmail.com>
2023-07-13 23:51:31 +0530

    get call back with another way

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,21 +1,21 @@
-// function callback(error, result) {
-//   if (error) {
-//     console.log("something went wrong");
-//   } else {
-//     console.log(result);
-//   }
-// }
+function callback(error, result) {
+  if (error) {
+    console.log("something went wrong");
+  } else {
+    console.log(result);
+  }
+}
 
-// function getData(key) {
-//   if (key === 971231) {
-//     const data = "Row data";
-//     callback(null, data);
-//   } else {
-//     callback("invalid key", null);
-//   }
-// }
+function getData(key) {
+  if (key === 971231) {
+    const data = "Row data";
+    callback(null, data);
+  } else {
+    callback("invalid key", null);
+  }
+}
 
-// getData(971231);
+getData(971231);
 
 const permission =()=>{
     console.log("hello...")
