04ed517b7093e630a54dc867fd98900fc01d8281
gmjishara <janith971231@gmail.com>
2023-07-14 11:50:49 +0530

    created a function that returns a promise

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,3 +1,5 @@
+// creating the promise constructor
+
 const promise = new Promise((resolve, reject) => {
   const randomNumber = Math.floor(Math.random() * 10);
   if (randomNumber > 5) {
@@ -10,3 +12,22 @@ const promise = new Promise((resolve, reject) => {
 promise
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
+
+// defining a function that returns a promise
+
+function getNumber(){
+  return new Promise((resolve, reject) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber > 5) {
+      resolve("The number is: " + randomNumber);
+    } else {
+      reject("Error! The number is less than 5");
+    }
+  });
+}
+
+function printNumber(num){
+  console.log(num);
+}
+
+getNumber().then(printNumber).catch(error=>console.log(error));
